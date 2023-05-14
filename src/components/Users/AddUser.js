f2e81339f1250d765489abc176cb96d938055c69
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-14 10:51:48 +0400

    adding user list file

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enterdeUsername, setEnterdeUsername] = useState("");
+  const [enterdeAge, setEnterdeAge] = useState("");
   const addUserHandler = (e) => {
     e.preventDefault();
+
+    if (enterdeAge.trim().length === 0 || enterdeUsername.trim().length === 0)
+      return;
+
+    if (+enterdeAge < 1) return;
+
+    setEnterdeAge("");
+    setEnterdeUsername("");
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnterdeUsername(e.target.value);
+  };
+  const ageChangeHandler = (e) => {
+    setEnterdeAge(e.target.value);
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enterdeUsername}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add user</button>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enterdeAge}
+        />
+        <Button type="submit">Add user</Button>
       </form>
     </Card>
   );
